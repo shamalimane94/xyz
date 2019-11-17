380e60d4f80887acc8d0a5cbf1e8f9017aae2ef7
shamalimane94 <shamalimane94@gmail.com>
2019-11-17 19:20:12 +0530

    shell commit

--- test.sh
+++ test.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
-
+echo "welcome shell..."
 if[$(docker container ls -q --filter name=app_db_container) != '']; then
 
 docker container stop app_db_container
 
-docker container rm app_db_container
+ docker container rm app_db_container
 
 fi
 
 if[$(docker image ls -q --filter reference=app_db) != ''];then
 
-docker image rm app_db
+ docker image rm app_db
+
 fi
 
 docker build -t app_db .
